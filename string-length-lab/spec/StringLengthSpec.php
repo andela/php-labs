3114c0423782055e3d93c7cc9a782eccf3445d03
andela-potemuyiwa <prosper.otemuyiwa@andela.co>
2015-07-30 19:49:46 +0100

    Add String Length lab

---
+++ string-length-lab/spec/StringLengthSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class StringLengthSpec extends ObjectBehavior
+{
+  function it_should_return_7_in_an_array_for_Prosper()
+  {
+    $this->getLength('Prosper')->shouldBeEqualTo([7]);
+  }
+
+  function it_should_return_3_in_an_array_for_Mia()
+  {
+    $this->getLength('Mia')->shouldBeEqualTo([3]);
+  }
+
+  function it_should_return_4_and_7_for_Adam_and_Frankel()
+  {
+    $this->getLength(['Adam','Frankel'])->shouldBeEqualTo([4,7]);
+  }
+}
